2d300e55fc57b014df6c5657603cab8bea9faa5d
Lucas Mateus Sureira <132959958+lucasaccardo@users.noreply.github.com>
2025-10-20 16:45:41 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- streamlit_app.py
+++ streamlit_app.py
@@ -600,7 +600,7 @@ if st.session_state.tela == "login":
     # Espaço antes do formulário
     st.markdown("<br><br>", unsafe_allow_html=True)
 
-    # Título moderno centralizado "Fleet Vamos SLA"
+    # Título moderno centralizado "Frotas Vamos SLA"
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
         st.markdown("<div class='brand-title'>Fleet Vamos SLA</div>", unsafe_allow_html=True)
@@ -1328,3 +1328,4 @@ else:
                             st.warning("Nenhuma peça foi selecionada.")
 
     st.markdown("</div>", unsafe_allow_html=True)
+
